1aea7554f3348088fd02466b6d204a741015b205
Khang V Phan <kphan000@uw.edu>
2019-06-30 20:04:16 -0500

    Fix some normalizing error

--- src/main/java/utils/StringNormalizer.java
+++ src/main/java/utils/StringNormalizer.java
@@ -6,12 +6,16 @@ import java.text.Normalizer.Form;
 public class StringNormalizer {
 	public static String NormalizeToASCII(String value) {
 		return Normalizer.normalize(value, Form.NFD)
+				.replaceAll("đ", "d")
+				.replaceAll("Đ", "D")
 				.replace("\u00a0", " ")
 				.replaceAll("[^\\p{ASCII}]", "");
 	}
 
 	public static String NormalizeToUrlFriendly(String value) {
 		return Normalizer.normalize(value, Form.NFD)
+				.replaceAll("đ", "d")
+				.replaceAll("Đ", "D")
 				.replace("\u00a0", " ")
 				.replaceAll("\\s+", "-")
 				.replaceAll("[^-a-zA-Z0-9]", "");
